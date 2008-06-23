3e09728a88c57291605909b2e9235ac5e843c01d
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-06-23 19:29:18 +0000

    First pass at resurrecting the Exhibit builder for the trunk.

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2008
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ * @subpackage ExhibitBuilder
+ **/
+ 
+define('EXHIBIT_BUILDER_VERSION', '0.2');
+
