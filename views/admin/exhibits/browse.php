4d02864497c1c835ab2f0224ca9b60a0b89f88ee
Dave Lester <davelester@gmail.com>
2008-10-13 19:33:10 +0000

    Fixes #451, Modifies exhibit builder navigation on browse page.

--- views/admin/exhibits/browse.php
+++ views/admin/exhibits/browse.php
@@ -26,6 +26,7 @@
 	<col id="col-id" />
 	<col id="col-title" />
 	<col id="col-tags" />
+	<col id="col-preview">
 	<col id="col-edit" />
 	<col id="col-delete" />
 	<thead>
@@ -34,6 +35,7 @@
 		<th>Title</th>
 		<th>Tags</th>
 		<th>Theme</th>
+		<th>Preview</th>
 		<?php if(has_permission('Exhibits','edit')){ ?>
 		
 		<th>Edit?</th>
@@ -44,12 +46,13 @@
 	</thead>
 	<tbody>
 		
-<?php foreach( $exhibits as $key=>$exhibit ): ?>
+<?php foreach($exhibits as $key=>$exhibit): ?>
 	<tr class="exhibit <?php if($key%2==1) echo ' even'; else echo ' odd'; ?>">
-		<td><?php echo h($exhibit->id);?></td>
-		<td><?php echo link_to_exhibit($exhibit); ?></td>
+		<td><?php echo $exhibit->id;?></td>
+		<td><?php echo $exhibit->title; ?></td>
 		<td><?php echo tag_string($exhibit, uri('exhibits/browse/tag/')); ?></td>
-		<td><?php echo h($exhibit->theme); ?></td>
+		<td><?php if($exhibit->theme==null) echo 'Current Public Theme'; else echo $exhibit->theme;?></td>
+		<td><?php echo '<a href="' .exhibit_uri($exhibit). '">[Preview]</a>'; ?></td>
 		<td><?php echo link_to($exhibit, 'edit', '[Edit]', array('class'=>'edit-exhibit')); ?></td>
 		<td><?php echo link_to($exhibit, 'delete', '[Delete]', array('class'=>'delete-exhibit')) ?></td>
 	</tr>
