fabee6f97c186946c62154c1581b0602873ed696
Dave Lester <davelester@gmail.com>
2008-10-28 19:11:47 +0000

    Fixes the display of section titles on exhibit form.

--- views/admin/exhibits/section-list.php
+++ views/admin/exhibits/section-list.php
@@ -4,8 +4,12 @@
     		<span class="left">
     			<span class="handle"><img src="<?php echo img('arrow_move.gif'); ?>" alt="Move" /></span>
     		<span class="input"><?php echo text(array('name'=>"Sections[$key][order]",'size'=>2,'class'=>'order-input'), $section->order); ?></span>
-
-    		<span class="section-title"><?php echo h($section->title); ?></span>
+    		<span class="section-title">
+    		<?php if (strlen($section->title) > 40):
+    		    echo htmlentities(substr($section->title, 0, 40));
+    		    else:
+    		    echo htmlentities($section->title); 
+    		    endif;?></span>
     		</span>
     		<span class="right">
     		<span class="section-edit"><a href="<?php echo uri('exhibits/edit-section/'.$section->id); ?>" class="edit">Edit</a></span>
