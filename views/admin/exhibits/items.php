81b7ed02472b946b37615cca2f58b46afe4e4a7d
Jeremy Boggs <jeremy@clioweb.org>
2008-11-04 05:57:24 +0000

    Styles pagination on page-content-form, moves pagination markup directly above items list.

--- views/admin/exhibits/items.php
+++ views/admin/exhibits/items.php
@@ -1,14 +1,3 @@
-<div id="pagination">
-<?php     
-	 echo pagination_links(array('url'=>uri(array('controller'=>'exhibits',
-      'action'=>'items', 'page'=>null)) . '/')); 
-     // The extra slash is a hack, the pagination should be fixed to work
-     // without the extra slash being there. Also, I get the feeling that being
-     // forced to set the 'page' parameter to null is also a hack.
-?>
-
-</div>
-
 <a href="#" id="show-or-hide-search" class="show-form">Show Search Form</a>
 <div id="page-search-form">
 <?php
@@ -20,7 +9,16 @@
 	//items_search_form(array('id'=>'search'), $uri); 
 ?>
 </div>
+<div id="pagination">
+<?php     
+	 echo pagination_links(array('url'=>uri(array('controller'=>'exhibits',
+      'action'=>'items', 'page'=>null)) . '/')); 
+     // The extra slash is a hack, the pagination should be fixed to work
+     // without the extra slash being there. Also, I get the feeling that being
+     // forced to set the 'page' parameter to null is also a hack.
+?>
 
+</div>
 <div id="item-list">
 <?php if (!has_items_for_loop()): ?>
     <p>There are no items to choose from.  Please refine your search or <a href="<?php echo uri('items/add'); ?>">add</a> some items.</p>
