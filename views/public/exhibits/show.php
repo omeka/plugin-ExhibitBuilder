99bcdd2582d608ed10bbc38bcf2e4bdec31ede8c
Jeremy Boggs <jeremy@clioweb.org>
2010-04-30 15:57:44 +0000

    Displays exhibit title in a link; Adds markup to display the title of an exhibit page; Moves exhibit page navigation to the bottom and wraps it in a containing div.

--- views/public/exhibits/show.php
+++ views/public/exhibits/show.php
@@ -1,16 +1,22 @@
 <?php head(array('title' => html_escape($exhibit->title),'bodyid'=>'exhibit','bodyclass'=>'show')); ?>
 <div id="primary">
-	<h2><?php echo html_escape($exhibit->title); ?></h2>
+	<h2><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h2>
 
-<div id="nav-container">
-	<?php echo exhibit_builder_section_nav();?>
-	<?php echo exhibit_builder_page_nav();?>
-</div>
+    <div id="nav-container">
+    	<?php echo exhibit_builder_section_nav();?>
+    	<?php echo exhibit_builder_page_nav();?>
+    </div>
 
 	<?php echo flash(); ?>
-	<?php echo exhibit_builder_link_to_previous_exhibit_page(); ?>
-	<?php echo exhibit_builder_link_to_next_exhibit_page(); ?>
+	<?php if($pageTitle = $page->title): ?>
+	<h3><?php echo $pageTitle; ?></h3>
+	<?php endif; ?>
 	
 	<?php exhibit_builder_render_exhibit_page(); ?>
+	
+	<div id="exhibit-page-navigation">
+	   	<?php echo exhibit_builder_link_to_previous_exhibit_page(); ?>
+    	<?php echo exhibit_builder_link_to_next_exhibit_page(); ?>
+	</div>
 </div>	
 <?php foot(); ?>
