f6de1ec17b4eb8ae7cd2b1ea570098b98ee9b30b
Jeremy Boggs <jeremy@clioweb.org>
2010-04-30 17:55:19 +0000

    Updates exhibit layout CSS to help them work better with themes by default.

--- views/shared/exhibit_layouts/gallery-full-left/layout.php
+++ views/shared/exhibit_layouts/gallery-full-left/layout.php
@@ -14,15 +14,16 @@
 			<?php echo exhibit_builder_link_to_exhibit_item('Item Link'); ?>
 		</div>
 		<?php endif; ?>
+		<?php if($text = exhibit_builder_page_text(1)):?>
+    	<div class="exhibit-text">
+    	    <?php echo $text; ?>
+    	</div>
+    	<?php endif; ?>
 	</div>
 	
 	<div class="secondary gallery">
-        <?php echo exhibit_builder_display_exhibit_thumbnail_gallery(2, 5, array('class'=>'permalink')); ?>
+        <?php echo exhibit_builder_display_exhibit_thumbnail_gallery(2, 12, array('class'=>'permalink')); ?>
 	</div>
 	
-	<?php if($text = exhibit_builder_page_text(1)):?>
-	<div class="tertiary">
-		<div class="exhibit-text"><?php echo exhibit_builder_page_text(1); ?></div>
-	</div>
-	<?php endif; ?>
+
 </div>
--- views/shared/exhibit_layouts/image-list-right-thumbs/layout.php
+++ views/shared/exhibit_layouts/image-list-right-thumbs/layout.php
@@ -5,15 +5,23 @@
 ?>
 
 <div class="image-list-right-thumbs">
-	<?php for ($i=1; $i <= 8; $i++): ?>
-		<?php if(exhibit_builder_use_exhibit_page_item($i)):?>
-		    <div class="exhibit-item">
-				<?php echo exhibit_builder_exhibit_display_item(array('imageSize'=>'thumbnail'), array('class'=>'permalink')); ?>
-				<div class="item-title"><?php echo item('Dublin Core', 'Title'); ?></div>
-				<?php if($text = exhibit_builder_page_text($i)): ?>
-		        <div class="exhibit-text"><?php echo $text; ?></div>
-				<?php endif; ?>
-		    </div>
+	<?php 
+	for ($i=1; $i <= 8; $i++): 
+	if(($text = exhibit_builder_page_text($i)) || exhibit_builder_use_exhibit_page_item($i)): ?>
+
+	<div class="image-text-group">
+
+	    <?php if($text): ?>
+        <div class="exhibit-text">
+            <?php echo $text; ?>
+        </div>
 		<?php endif; ?>
-	<?php endfor;?>
+		<?php if(exhibit_builder_use_exhibit_page_item($i)):?>
+	    <div class="exhibit-item">
+			<?php echo exhibit_builder_exhibit_display_item(array('imageSize'=>'thumbnail'), array('class'=>'permalink')); ?>
+			<?php echo item('Dublin Core', 'Title'); ?>
+	    </div>
+		<?php endif; ?>
+	</div>
+	<?php endif; endfor;?>
 </div>
