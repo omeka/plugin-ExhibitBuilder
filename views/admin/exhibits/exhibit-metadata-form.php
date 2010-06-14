e71259c0f48295fb775da040d8271a2539e8f70d
John Flatness <john@zerocrates.org>
2010-06-14 20:32:51 +0000

    Improves metadata form readability.

--- views/admin/exhibits/exhibit-metadata-form.php
+++ views/admin/exhibits/exhibit-metadata-form.php
@@ -58,58 +58,65 @@ if ($exhibit->title) {
         <a href="<?php echo html_escape(uri('exhibits')); ?>">Exhibits</a> &gt; <?php echo html_escape($actionName . ' Exhibit'); ?>
     </div>
 
-<form id="exhibit-metadata-form" method="post" class="exhibit-builder">
+<?php echo flash();?>
 
-    <fieldset>
-        <legend>Exhibit Metadata</legend>
-        <?php echo flash();?>
-    <div class="field">
-    <?php echo text(array('name'=>'title', 'class'=>'textinput', 'id'=>'title'), $exhibit->title, 'Exhibit Title'); ?>
-    <?php echo form_error('title'); ?>
-    </div>
-    <div class="field"><?php echo text(array('name'=>'slug', 'id'=>'slug', 'class'=>'textinput'), $exhibit->slug, 'Exhibit Slug (no spaces or special characters)'); ?>
-    <?php echo form_error('slug'); ?>
-    </div>
-    <div class="field"><?php echo text(array('name'=>'credits', 'id'=>'credits', 'class'=>'textinput'), $exhibit->credits,'Exhibit Credits'); ?></div>
-    <div class="field"><?php echo textarea(array('name'=>'description', 'id'=>'description', 'class'=>'textinput','rows'=>'10','cols'=>'40'), $exhibit->description, 'Exhibit Description'); ?></div>   
-    <?php $exhibitTagList = join(', ', pluck('name', $exhibit->Tags)); ?>
-    <div class="field"><?php echo text(array('name'=>'tags', 'id'=>'tags', 'class'=>'textinput'), $exhibitTagList, 'Exhibit Tags'); ?></div>
-    <div class="field">
-        <label for="featured">Exhibit is featured:</label>
-        <div class="radio"><?php echo checkbox(array('name'=>'featured', 'id'=>'featured'), $exhibit->featured); ?></div>
-    </div>
-    
-    <div class="field">
-        <label for="featured">Exhibit is public:</label>
-        <div class="radio"><?php echo checkbox(array('name'=>'public', 'id'=>'public'), $exhibit->public); ?></div>
-    </div>
-        <div class="field">
-            <label for="theme">Exhibit Theme</label>            
-            <?php $values = array('' => 'Current Public Theme') + exhibit_builder_get_ex_themes(); ?>
-            <div class="select"><?php echo __v()->formSelect('theme', $exhibit->theme, array('id'=>'theme'), $values); ?>
-            <?php if ($theme && $theme->hasConfig): ?><a href="<?php echo html_escape(uri("exhibits/theme-config/$exhibit->id")); ?>" class="configure-button button">Configure</a><?php endif;?>
-            </div>
-        </div>
-        </fieldset>
-    <fieldset>
-        <legend>Exhibit Sections</legend>
-        
-        <div id="section-list-container">
-            <?php if (!$exhibit->Sections): ?>
-                <p>There are no sections.</p>
-            <?php else: ?>
-            <ol id="section-list">
-                <?php common('section-list', compact('exhibit'), 'exhibits'); ?>
-            </ol>
-            <?php endif;?>
-        </div>
-        </fieldset>
-        
+    <form id="exhibit-metadata-form" method="post" class="exhibit-builder">
+
         <fieldset>
-        <p id="exhibit-builder-save-changes"><input type="submit" name="save_exhibit" id="save_exhibit" value="Save Changes" /> or 
-            <input type="submit" name="add_section" value="Add Section" /> or 
-            <a href="<?php echo html_escape(uri('exhibits')); ?>" class="cancel">Cancel</a></p>
+            <legend>Exhibit Metadata</legend>
+            <div class="field">
+            <?php echo text(array('name'=>'title', 'class'=>'textinput', 'id'=>'title'), $exhibit->title, 'Exhibit Title'); ?>
+            <?php echo form_error('title'); ?>
+            </div>
+            <div class="field">
+            <?php echo text(array('name'=>'slug', 'id'=>'slug', 'class'=>'textinput'), $exhibit->slug, 'Exhibit Slug (no spaces or special characters)'); ?>
+            <?php echo form_error('slug'); ?>
+            </div>
+            <div class="field">
+            <?php echo text(array('name'=>'credits', 'id'=>'credits', 'class'=>'textinput'), $exhibit->credits,'Exhibit Credits'); ?>
+            </div>
+            <div class="field">
+            <?php echo textarea(array('name'=>'description', 'id'=>'description', 'class'=>'textinput','rows'=>'10','cols'=>'40'), $exhibit->description, 'Exhibit Description'); ?>
+            </div>   
+            <?php $exhibitTagList = join(', ', pluck('name', $exhibit->Tags)); ?>
+            <div class="field">
+            <?php echo text(array('name'=>'tags', 'id'=>'tags', 'class'=>'textinput'), $exhibitTagList, 'Exhibit Tags'); ?>
+            </div>
+            <div class="field">
+                <label for="featured">Exhibit is featured:</label>
+                <div class="radio"><?php echo checkbox(array('name'=>'featured', 'id'=>'featured'), $exhibit->featured); ?></div>
+            </div>
+            <div class="field">
+                <label for="featured">Exhibit is public:</label>
+                <div class="radio"><?php echo checkbox(array('name'=>'public', 'id'=>'public'), $exhibit->public); ?></div>
+            </div>
+            <div class="field">
+                <label for="theme">Exhibit Theme</label>            
+                <?php $values = array('' => 'Current Public Theme') + exhibit_builder_get_ex_themes(); ?>
+                <div class="select"><?php echo __v()->formSelect('theme', $exhibit->theme, array('id'=>'theme'), $values); ?>
+                <?php if ($theme && $theme->hasConfig): ?><a href="<?php echo html_escape(uri("exhibits/theme-config/$exhibit->id")); ?>" class="configure-button button">Configure</a><?php endif;?>
+                </div>
+            </div>
         </fieldset>
-</form>     
+        <fieldset>
+            <legend>Exhibit Sections</legend>
+            <div id="section-list-container">
+                <?php if (!$exhibit->Sections): ?>
+                    <p>There are no sections.</p>
+                <?php else: ?>
+                <ol id="section-list">
+                    <?php common('section-list', compact('exhibit'), 'exhibits'); ?>
+                </ol>
+                <?php endif;?>
+            </div>
+        </fieldset>
+        <fieldset>
+            <p id="exhibit-builder-save-changes">
+                <input type="submit" name="save_exhibit" id="save_exhibit" value="Save Changes" /> or 
+                <input type="submit" name="add_section" value="Add Section" /> or 
+                <a href="<?php echo html_escape(uri('exhibits')); ?>" class="cancel">Cancel</a>
+            </p>
+        </fieldset>
+    </form>     
 </div>
 <?php foot(); ?>
