8cf1d7f03699a85b39ea5e3463291fc655133c11
John Flatness <john@zerocrates.org>
2010-06-14 14:58:53 +0000

    Fixes bug where changing EB logo deletes global logo.

--- controllers/ExhibitsController.php
+++ controllers/ExhibitsController.php
@@ -295,14 +295,15 @@ class ExhibitBuilder_ExhibitsController extends Omeka_Controller_Action
                             $newFileName = $uploadedFileNames[$elementName];
                             $formValues[$elementName] = $newFileName;
                             
+                            // Temporarily remove this removal logic until a better solution can be found.
                             // delete old file if it is not the same as the new file name
-                            $oldFileName = $currentThemeOptions[$elementName];
+                            /*$oldFileName = $currentThemeOptions[$elementName];
                             if ($oldFileName != $newFileName) {
                                 $oldFilePath = THEME_UPLOADS_DIR . DIRECTORY_SEPARATOR . $oldFileName;
                                 if (is_writable($oldFilePath) && is_file($oldFilePath)) {
                                     unlink($oldFilePath);
                                 }
-                            }
+                            }*/
                         }       
                     } else if ($element instanceof Zend_Form_Element_Hidden) {
                         $elementName = $element->getName();
