d3ed30382acec6ba200fe4942e7ed82dfb02dc81
John Flatness <john@zerocrates.org>
2010-07-09 19:25:15 +0000

    Fixes dumb missing quotes in define.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php 
-define(EXHIBIT_BUILDER_DIR, dirname(dirname(__FILE__)));
+define('EXHIBIT_BUILDER_DIR', dirname(dirname(__FILE__)));
 $baseBootstrap = dirname(dirname(dirname(dirname(__FILE__)))) . '/application/tests/bootstrap.php';
 require_once $baseBootstrap;
-require_once 'ExhibitBuilder_TestCase.php';
-require_once 'ExhibitBuilder_ViewTestCase.php';
+require_once 'ExhibitBuilder_TestCase.php';require_once 'ExhibitBuilder_ViewTestCase.php';
