2d3aa82a41a12b45c6152e9444658c4ff1b8c6bb
Jeremy Boggs <jeremy@clioweb.org>
2010-09-17 17:17:52 +0000

    Fixes #132 HTML for exhibit descriptions is being escaped.

--- helpers/ExhibitFunctions.php
+++ helpers/ExhibitFunctions.php
@@ -682,7 +682,7 @@ function exhibit($propertyName, $options = array(), $exhibit = null)
     }
     $propertyName = Inflector::underscore($propertyName);
 	if (property_exists(get_class($exhibit), $propertyName)) {
-	    return html_escape($exhibit->$propertyName);
+	    return $exhibit->$propertyName;
 	} else {
 	    return null;
 	}
