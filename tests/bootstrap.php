f7a83101cb356320a05f31c65d41802a6c228f24
John Flatness <john@zerocrates.org>
2012-01-11 16:20:29 -0500

    Tweak bootstrap for running all tests through Ant.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * @version $Id$
- * @copyright Center for History and New Media, 2007-2010
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @package Omeka
- */
 
-require_once '../../../application/tests/bootstrap.php';
 define('EXHIBIT_BUILDER_DIR', dirname(dirname(__FILE__)));
+require_once dirname(dirname(EXHIBIT_BUILDER_DIR)) . '/application/tests/bootstrap.php';
 
 require_once 'ExhibitBuilder_ViewTestCase.php';
 require_once 'ExhibitBuilder_IntegrationHelper.php';
-
