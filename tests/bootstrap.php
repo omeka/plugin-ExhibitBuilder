79a819913c45546163e2047d49f3ba303979f922
John Flatness <john@zerocrates.org>
2012-01-11 11:48:58 -0500

    Fix tests for PHPUnit 3.6.

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+define('EXHIBIT_BUILDER_DIR', dirname(dirname(__FILE__)));
+require_once dirname(dirname(EXHIBIT_BUILDER_DIR)) . '/application/tests/bootstrap.php';
+
+require_once 'ExhibitBuilder_ViewTestCase.php';
+require_once 'ExhibitBuilder_IntegrationHelper.php';
+
