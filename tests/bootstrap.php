058f5cfd0a632da55515e035162f8683539d9599
John Flatness <john@zerocrates.org>
2012-01-11 11:53:32 -0500

    Don't do dirname when .. will do.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,8 @@
  * @package Omeka
  */
 
+require_once '../../../application/tests/bootstrap.php';
 define('EXHIBIT_BUILDER_DIR', dirname(dirname(__FILE__)));
-require_once dirname(dirname(EXHIBIT_BUILDER_DIR)) . '/application/tests/bootstrap.php';
 
 require_once 'ExhibitBuilder_ViewTestCase.php';
 require_once 'ExhibitBuilder_IntegrationHelper.php';
