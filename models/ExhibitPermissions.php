bca5877dbbaed519f5f97baa8f0718957c2ef2fc
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-02-27 12:56:24 -0500

    Makes compatible with 2.0 Acl

--- models/ExhibitPermissions.php
+++ models/ExhibitPermissions.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
  * This encapsulates the permissions check for an exhibit.
  * @todo Find a way to hook this into the plugins
- * 
+ *
  * @version $Id$
  * @copyright Center for History and New Media, 2007-2009
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
@@ -22,11 +22,11 @@ class ExhibitPermissions
         if (version_compare(OMEKA_VERSION, '2.0-dev', '>=')) {
             $oc = Omeka_Context::getInstance();
             $acl = $oc->getAcl();
-            $currentUser = $oc->getCurrentUser();        
+            $currentUser = $oc->getCurrentUser();
             $hasPermission = $acl->isAllowed($currentUser, 'ExhibitBuilder_Exhibits', 'showNotPublic');
         } else {
             $acl = Omeka_Context::getInstance()->getAcl();
-            $hasPermission = $acl->checkUserPermission('ExhibitBuilder_Exhibits', 'showNotPublic');            
+            $hasPermission = $acl->isAllowed(current_user(), 'ExhibitBuilder_Exhibits', 'showNotPublic');
         }
 
         if(!$hasPermission)
