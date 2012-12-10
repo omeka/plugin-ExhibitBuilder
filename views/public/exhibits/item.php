fd16e35e2628521cdbe8bfd652b6130428494028
Kim Nguyen <design@kimisgold.com>
2012-12-10 16:14:47 -0500

    Use spaces instead of tabs.

--- views/public/exhibits/item.php
+++ views/public/exhibits/item.php
@@ -4,32 +4,32 @@ echo head(array('title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'exhibi
 ?>
 <div id="primary">
 
-	<h1 class="item-title"><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
-	
-	<?php echo show_item_metadata(); ?>
-	
-	<div id="itemfiles">
-		<?php echo files_for_item(); ?>
-	</div>
-	
-	<?php if (metadata('item', 'Collection Name')): ?>
+    <h1 class="item-title"><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+    
+    <?php echo show_item_metadata(); ?>
+    
+    <div id="itemfiles">
+        <?php echo files_for_item(); ?>
+    </div>
+    
+    <?php if (metadata('item', 'Collection Name')): ?>
         <div id="collection" class="field">
             <h2><?php echo __('Collection'); ?></h2>
             <div class="field-value"><p><?php echo link_to_collection_for_item(); ?></p></div>
         </div>
     <?php endif; ?>
     
-	<?php if (metadata('item', 'has tags')): ?>
-	<div class="tags">
-		<h2><?php echo __('Tags'); ?></h2>
-	   <?php echo tag_string('item'); ?>
-	</div>
-	<?php endif;?>
-	
-	<div id="citation" class="field">
-    	<h2><?php echo __('Citation'); ?></h2>
-    	<p id="citation-value" class="field-value"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></p>
-	</div>
-	
+    <?php if (metadata('item', 'has tags')): ?>
+      <div class="tags">
+        <h2><?php echo __('Tags'); ?></h2>
+       <?php echo tag_string('item'); ?>
+    </div>
+    <?php endif;?>
+    
+    <div id="citation" class="field">
+        <h2><?php echo __('Citation'); ?></h2>
+        <p id="citation-value" class="field-value"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></p>
+    </div>
+    
 </div>
 <?php echo foot(); ?>
