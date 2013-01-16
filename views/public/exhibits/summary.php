669350de0e009181a6327444504e08b2e8c3daad
John Flatness <john@zerocrates.org>
2013-01-16 12:03:46 -0500

    Remove some unneeded labels from the summary.

--- views/public/exhibits/summary.php
+++ views/public/exhibits/summary.php
@@ -3,11 +3,16 @@
 <h1><?php echo metadata('exhibit', 'title'); ?></h1>
 <?php echo exhibit_builder_page_nav(); ?>
 
-<h2><?php echo __('Description'); ?></h2>
-<?php echo metadata('exhibit', 'description', array('no_escape' => true)); ?>
+<div class="exhibit-description">
+    <?php echo metadata('exhibit', 'description', array('no_escape' => true)); ?>
+</div>
 
-<h2><?php echo __('Credits'); ?></h2>
-<p><?php echo metadata('exhibit', 'credits'); ?></p>
+<div class="exhibit-credits">
+    <?php if (($credits = metadata('exhibit', 'credits'))): ?>
+    <h2><?php echo __('Credits'); ?></h2>
+    <p><?php echo $credits; ?></p>
+    <?php endif; ?>
+</div>
 
 <ul id="exhibit-pages">
     <?php set_exhibit_pages_for_loop_by_exhibit(); ?>
