aaf7fb3c65fa95a7edb534205a813dfc7692b6c5
Kim Nguyen <design@kimisgold.com>
2013-04-25 14:21:23 -0400

    Update secondary-nav classes.

--- views/public/exhibits/browse.php
+++ views/public/exhibits/browse.php
@@ -5,7 +5,7 @@ echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
 <h1><?php echo $title; ?> <?php echo __('(%s total)', $total_results); ?></h1>
 <?php if (count($exhibits) > 0): ?>
 
-<nav class="navigation" id="secondary-nav">
+<nav class="navigation secondary-nav">
     <?php echo nav(array(
         array(
             'label' => __('Browse All'),
