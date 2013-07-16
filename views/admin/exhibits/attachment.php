2abc8a4438e0fdb10a7c13cedcf12aa30447dd6f
John Flatness <john@zerocrates.org>
2013-07-16 17:07:26 -0400

    Add thumbs to attachments.

--- views/admin/exhibits/attachment.php
+++ views/admin/exhibits/attachment.php
@@ -8,6 +8,9 @@ $stem = $block->getFormStem() . "[attachments][{$index}]";
         #<?php echo html_escape($item->id); ?>:
         <?php echo metadata($item, array('Dublin Core', 'Title')); ?>
     </a></h5>
+    <?php if ($file): ?>
+    <?php echo file_image('square_thumbnail', array(), $file); ?>
+    <?php endif; ?>
     <?php echo $this->formHidden($stem . '[item_id]', $item->id); ?>
     <?php echo $this->formHidden($stem . '[file_id]', $file->id); ?>
     <?php echo $this->formHidden($stem . '[caption]', $attachment->caption); ?>
