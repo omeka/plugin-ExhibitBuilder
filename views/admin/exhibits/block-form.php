00d1d3abcede648c46c9d6c075a4726d6bb3b048
Kim Nguyen <design@kimisgold.com>
2013-07-17 17:59:14 -0400

    Add headings for each content block.

--- views/admin/exhibits/block-form.php
+++ views/admin/exhibits/block-form.php
@@ -1,9 +1,10 @@
 <?php
 $layout = $block->getLayout();
 $stem = $block->getFormStem();
+$order = $block->order;
 ?>
-<div class="block-form" data-block-index="<?php echo $block->order; ?>">
-<a href="#" class="remove-block"><?php echo __('Remove'); ?></a>
+<div class="block-form" data-block-index="<?php echo $order; ?>">
+<h2><?php echo __('Section'); ?> <?php echo $order; ?></h2>
 <a href="#" class="remove-block"><span class="screen-reader-text"><?php echo __('Remove'); ?></span></a>
 <?php echo $this->formHidden($stem . '[layout]', $block->layout); ?>
 <?php echo $this->formHidden($stem . '[order]', $block->order); ?>
