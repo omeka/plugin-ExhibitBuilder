9226009a40b6d39c57be9d8b9d515383456b12b9
Kim Nguyen <design@kimisgold.com>
2013-07-23 15:00:40 -0400

    In item search, move item title after file thumbnails, and only display one thumbnail.

--- views/helpers/ExhibitItemListing.php
+++ views/helpers/ExhibitItemListing.php
@@ -12,19 +12,20 @@ class ExhibitBuilder_View_Helper_ExhibitItemListing extends Zend_View_Helper_Abs
      */
     public function exhibitItemListing($item)
     {
-        $html = '<div class="item-listing" data-item-id="' . $item->id . '">'
-              . '<h4 class="title">'
-              . metadata($item, array('Dublin Core', 'Title'))
-              . '</h4>';
+        $html = '<div class="item-listing" data-item-id="' . $item->id . '">';
         if (metadata($item, 'has files')) {
             foreach ($item->Files as $displayFile) {
                 if ($displayFile->hasThumbnail()) {
                     $html .= '<div class="item-file">'
                         . file_image('square_thumbnail', array(), $displayFile)
                         . '</div>';
+                    break;
                 }
             }
         }
+        $html .= '<h4 class="title">'
+              . metadata($item, array('Dublin Core', 'Title'))
+              . '</h4>';
         $html .= '<button type="button" class="select-item">' . __('Select Item') . '</button>';
         $html .= '</div>';
         return $html;
