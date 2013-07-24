8f8c4e76ec6dd7b16691b94551840bea8ed9d9c9
John Flatness <john@zerocrates.org>
2013-07-24 14:57:32 -0400

    Add size setting for file-text.

--- views/shared/exhibit_layouts/file-text/form.php
+++ views/shared/exhibit_layouts/file-text/form.php
@@ -4,8 +4,24 @@ $options = $block->getOptions();
 ?>
 <div class="file-position">
     <?php echo $this->formLabel($formStem . '[options][file-position]', __('File position')); ?>
-    <?php $file_position = array('left' => 'Left', 'right' => 'Right', 'center' => 'Center (no text wrap)'); ?>
-    <?php echo $this->formSelect($formStem . '[options][file-position]', @$options['file-position'], array(), $file_position); ?>
+    <?php
+    echo $this->formSelect($formStem . '[options][file-position]',
+        @$options['file-position'], array(),
+        array('left' => 'Left', 'right' => 'Right'));
+    ?>
+</div>
+
+<div class="file-size">
+    <?php echo $this->formLabel($formStem . '[options][file-size]', __('File size')); ?>
+    <?php
+    echo $this->formSelect($formStem . '[options][file-size]',
+        @$options['file-size'], array(),
+        array(
+            'fullsize' => 'Fullsize',
+            'thumbnail' => 'Thumbnail',
+            'square_thumbnail' => 'Square Thumbnail'
+        ));
+    ?>
 </div>
 
 <div class="selected-items">
--- views/shared/exhibit_layouts/file-text/layout.php
+++ views/shared/exhibit_layouts/file-text/layout.php
@@ -1,6 +1,14 @@
-<div class="exhibit-items <?php echo html_escape($options['file-position']); ?>">
+<?php
+$position = isset($options['file-position'])
+    ? html_escape($options['file-position'])
+    : 'left';
+$size = isset($options['file-size'])
+    ? html_escape($options['file-size'])
+    : 'fullsize';
+?>
+<div class="exhibit-items <?php echo $position; ?> <?php echo $size; ?>">
     <?php foreach ($attachments as $attachment): ?>
-        <?php echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize')); ?>
+        <?php echo $this->exhibitAttachment($attachment, array('imageSize' => $size)); ?>
     <?php endforeach; ?>
 </div>
 <?php echo $text; ?>
