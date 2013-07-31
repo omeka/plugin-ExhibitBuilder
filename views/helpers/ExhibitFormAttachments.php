404b1baf7f8fa6f60c19fa5d2775926b0dc024e2
Kim Nguyen <design@kimisgold.com>
2013-07-31 17:04:13 -0400

    Capitalize button text for consistency.

--- views/helpers/ExhibitFormAttachments.php
+++ views/helpers/ExhibitFormAttachments.php
@@ -27,7 +27,7 @@ class ExhibitBuilder_View_Helper_ExhibitFormAttachments extends Zend_View_Helper
         }
 
         $html .= '</div>';
-        $html .= '<div class="add-item button"><a href="#">Add item</a></div>';
+        $html .= '<div class="add-item button"><a href="#">Add Item</a></div>';
         return $html;
     }
 }
