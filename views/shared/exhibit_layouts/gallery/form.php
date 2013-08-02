bb1ee2bcf5fd277403aecad96c4ce57aa8e90f99
Kim Nguyen <design@kimisgold.com>
2013-08-02 15:31:56 -0400

    Make drawer markup consistent for gallery layout form as well.

--- views/shared/exhibit_layouts/gallery/form.php
+++ views/shared/exhibit_layouts/gallery/form.php
@@ -17,7 +17,7 @@ $options = $block->getOptions();
 
     <h4><?php echo __('Layout Options'); ?></h4>
     
-    <a href="#" class="drawer"><span class="screen-reader-text"><?php echo __('Expand'); ?></span></a>
+    <div class="drawer"></div>
 
     <div class="aside-position">
         <?php echo $this->formLabel($formStem . '[options][aside-position]', __('Aside position')); ?>
