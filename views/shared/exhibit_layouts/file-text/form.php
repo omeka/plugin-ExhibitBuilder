abd611c87837db391bd98e67f789cfe2118c7dc5
Kim Nguyen <design@kimisgold.com>
2013-08-23 17:25:17 -0400

    Generalize instruction class, tweak margin.

--- views/shared/exhibit_layouts/file-text/form.php
+++ views/shared/exhibit_layouts/file-text/form.php
@@ -5,7 +5,7 @@ $options = $block->getOptions();
 
 <div class="selected-items">
     <h4><?php echo __('Items'); ?></h4>
-    <p class="reorder-instructions"><?php echo __('To reorder items, click and drag the item to the preferred location.'); ?></p>
+    <p class="instructions"><?php echo __('To reorder items, click and drag the item to the preferred location.'); ?></p>
     <?php echo $this->exhibitFormAttachments($block); ?>
 </div>
 
