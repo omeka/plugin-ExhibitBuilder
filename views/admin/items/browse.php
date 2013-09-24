680b98b5d9188806e3bd7fd9ac5cc779467bd960
John Flatness <john@zerocrates.org>
2013-09-24 15:05:29 -0400

    Show search filters in item list.
    Fix problem with hiding filters and pushing down page nav.

--- views/admin/items/browse.php
+++ views/admin/items/browse.php
@@ -1,9 +1,9 @@
-<?php echo item_search_filters(); ?>
 <?php
 echo pagination_links(array('url' => url(array('controller' => 'exhibits',
     'action'=>'items', 'page' => null)))); 
 ?>
 <div id="item-list">
+<?php echo item_search_filters(); ?>
 <?php if (!has_loop_records('items')): ?>
     <p><?php echo __('There are no items to choose from.  Please refine your search or %s.', '<a href="' . html_escape(url('items/add')) .'">' . __('add some items') .'</a>') ?></p>
 <?php endif; ?>
