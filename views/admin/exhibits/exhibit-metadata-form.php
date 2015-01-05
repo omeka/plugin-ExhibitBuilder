d9148a73a64acb2cb68fbfc5eb49bdb6bb0b5f4c
John Kloor <john@kloor.org>
2015-01-05 08:52:42 -0500

    Check for delete permission before showing button.
    If the user does not have permission to delete an Exhibit, the button
    will not work anyway. Therefore, the button does not need to be
    displayed for those users.

--- views/admin/exhibits/exhibit-metadata-form.php
+++ views/admin/exhibits/exhibit-metadata-form.php
@@ -76,7 +76,9 @@
             <?php echo $this->formSubmit('save_exhibit', __('Save Changes'), array('class'=>'submit big green button')); ?>
             <?php if ($exhibit->exists()): ?>
                 <?php echo exhibit_builder_link_to_exhibit($exhibit, __('View Public Page'), array('class' => 'big blue button', 'target' => '_blank')); ?>
-                <?php echo link_to($exhibit, 'delete-confirm', __('Delete'), array('class' => 'big red button delete-confirm')); ?>
+                <?php if (is_allowed($exhibit, 'delete')): ?>
+                    <?php echo link_to($exhibit, 'delete-confirm', __('Delete'), array('class' => 'big red button delete-confirm')); ?>
+                <?php endif; ?>
             <?php endif; ?>
             <div id="public-featured">
                 <div class="public">
