2f96c8b2cc23ff7d2b3a4efbfe7bc40a080bdfd7
John Kloor <john@kloor.org>
2015-01-29 16:13:38 -0500

    Fix captions alignment in galleries.

--- views/shared/exhibit_layouts/gallery/layout.php
+++ views/shared/exhibit_layouts/gallery/layout.php
@@ -14,14 +14,14 @@ $captionPosition = isset($options['captions-position'])
     : 'center';
 ?>
 <?php if ($showcaseFile): ?>
-<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
+<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?> captions-<?php echo $captionPosition; ?>">
     <?php
         $attachment = array_shift($attachments);
         echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize'));
     ?>
 </div>
 <?php endif; ?>
-<div class="gallery <?php if ($showcaseFile || !empty($text)) echo "with-showcase $galleryPosition captions-$captionPosition"; ?>">
+<div class="gallery <?php if ($showcaseFile || !empty($text)) echo "with-showcase $galleryPosition"; ?> captions-<?php echo $captionPosition; ?>">
     <?php echo $this->exhibitAttachmentGallery($attachments, array('imageSize' => $galleryFileSize)); ?>
 </div>
 <?php echo $text; ?>
