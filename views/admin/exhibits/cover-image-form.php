95f133e456de592712d5054284269ffb1b7a1494
Will Simpson <wsimpson@email.arizona.edu>
2015-05-11 16:56:09 -0700

    Add AJAX partial form for filtering Exhibit cover images.

---
+++ views/admin/exhibits/cover-image-form.php
@@ -0,0 +1,16 @@
+<form id="cover-image-form" action="<?php echo $formActionUri; ?>">
+    <div class="field">
+        <?php
+        echo $this->formLabel('exhibit-page-search', __('Filter By Page'));?>
+        <div class="inputs">
+        <?php 
+            echo $this->formSelect(
+                'exhibit-page',
+                @$_REQUEST['exhibit-page'],
+                array('id' => 'exhibit-page'),
+                get_table_options('ExhibitPage', null, array('exhibit_id'=>$exhibit->id))
+            );
+        ?>
+        </div>
+    </div>
+</form>
