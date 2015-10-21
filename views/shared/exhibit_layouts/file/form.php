2a8762b4c6089302966b928cfe5f73ccf5ccec50
Kim Nguyen <design@kimisgold.com>
2015-10-20 20:51:28 -0400

    Add standalone file layout.

---
+++ views/shared/exhibit_layouts/file/form.php
@@ -0,0 +1,51 @@
+<?php
+$formStem = $block->getFormStem();
+$options = $block->getOptions();
+?>
+<div class="selected-items">
+    <h4><?php echo __('Items'); ?></h4>
+    <?php echo $this->exhibitFormAttachments($block); ?>
+</div>
+
+<div class="layout-options">
+    <div class="block-header">
+        <h4><?php echo __('Layout Options'); ?></h4>
+        <div class="drawer"></div>
+    </div>
+
+    <div class="file-position">
+        <?php echo $this->formLabel($formStem . '[options][file-position]', __('File position')); ?>
+        <?php
+        echo $this->formSelect($formStem . '[options][file-position]',
+            @$options['file-position'], array(),
+            array('left' => __('Left'), 'right' => __('Right'), 'center' => __('Center')));
+        ?>
+    </div>
+
+    <div class="file-size">
+        <?php echo $this->formLabel($formStem . '[options][file-size]', __('File size')); ?>
+        <?php
+        echo $this->formSelect($formStem . '[options][file-size]',
+            @$options['file-size'], array(),
+            array(
+                'fullsize' => __('Fullsize'),
+                'thumbnail' => __('Thumbnail'),
+                'square_thumbnail' => __('Square Thumbnail')
+            ));
+        ?>
+    </div>
+
+    <div class="captions-position">
+        <?php echo $this->formLabel($formStem . '[options][captions-position]', __('Captions position')); ?>
+        <?php
+        echo $this->formSelect($formStem . '[options][captions-position]',
+            @$options['captions-position'], array(),
+            array(
+                'center' => __('Center'),
+                'left' => __('Left'),
+                'right' => __('Right')
+            ));
+        ?>
+    </div>
+
+</div>
