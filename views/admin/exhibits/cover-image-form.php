7584a7bbebb46931533dccdc54d1f12369743747
Will Simpson <wsimpson@email.arizona.edu>
2015-11-16 12:51:08 -0700

    Removed commented out code.

--- views/admin/exhibits/cover-image-form.php
+++ views/admin/exhibits/cover-image-form.php
@@ -1,18 +1,7 @@
 <form id="cover-image-form" action="<?php echo $formActionUri; ?>">
     <div class="field">
         <div class="column filter-left">
-            <?php
-            echo $this->formLabel('exhibit-page-search', __('Filter By Page'));?>
-        </div>
-        <div class="column filter-right">
-        <?php
-            echo $this->formSelect(
-                'exhibit-page',
-                @$_REQUEST['exhibit-page'],
-                array('id' => 'exhibit-page'),
-                get_table_options('ExhibitPage', null, array('exhibit_id'=>$exhibit->id))
-            );
-        ?>
+            <?php echo $this->formLabel('exhibit-page-search', __('Filter By Page'));?>
         </div>
     </div>
 </form>
