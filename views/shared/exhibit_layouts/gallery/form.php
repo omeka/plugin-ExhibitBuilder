7c3152e323f2499ddab25797545c534b47940e95
Kim Nguyen <design@kimisgold.com>
2022-05-04 12:59:44 -0400

    Move closing div tag to contain all gallery layout options.

--- views/shared/exhibit_layouts/gallery/form.php
+++ views/shared/exhibit_layouts/gallery/form.php
@@ -32,46 +32,46 @@ $options = $block->getOptions();
             );
             ?>
         </div>
-    </div>
 
-    <div class="gallery-position">
-        <?php echo $this->formLabel($formStem . '[options][gallery-position]', __('Gallery position')); ?>
-        <?php
-        echo $this->formSelect($formStem . '[options][gallery-position]',
-            @$options['gallery-position'], array(),
-            array(
-                'left' => __('Left'),
-                'right' => __('Right')
-            )
-        );
-        ?>
-        <p class="instructions"><?php echo __('If there is no showcase file or text, the gallery will use the full width of the page.'); ?></p>
-    </div>
+        <div class="gallery-position">
+            <?php echo $this->formLabel($formStem . '[options][gallery-position]', __('Gallery position')); ?>
+            <?php
+            echo $this->formSelect($formStem . '[options][gallery-position]',
+                @$options['gallery-position'], array(),
+                array(
+                    'left' => __('Left'),
+                    'right' => __('Right')
+                )
+            );
+            ?>
+            <p class="instructions"><?php echo __('If there is no showcase file or text, the gallery will use the full width of the page.'); ?></p>
+        </div>
 
-    <div class="gallery-file-size">
-        <?php echo $this->formLabel($formStem . '[options][gallery-file-size]', __('Gallery file size')); ?>
-        <?php
-            $defaultFileSize = (get_option('use_square_thumbnail') == 1) ? 'square_thumbnail' : 'thumbnail';
-            echo $this->formSelect($formStem . '[options][gallery-file-size]',
-            (@$options['gallery-file-size']) ? @$options['gallery-file-size'] : $defaultFileSize, array(),
-            array(
-                'square_thumbnail' => __('Square Thumbnail'),
-                'thumbnail' => __('Thumbnail'),
-            ));
-            
-        ?>
-    </div>
-    
-    <div class="captions-position">
-        <?php echo $this->formLabel($formStem . '[options][captions-position]', __('Captions position')); ?>
-        <?php
-        echo $this->formSelect($formStem . '[options][captions-position]',
-            @$options['captions-position'], array(),
-            array(
-                'center' => __('Center'),
-                'left' => __('Left'),
-                'right' => __('Right')
-            ));
-        ?>
+        <div class="gallery-file-size">
+            <?php echo $this->formLabel($formStem . '[options][gallery-file-size]', __('Gallery file size')); ?>
+            <?php
+                $defaultFileSize = (get_option('use_square_thumbnail') == 1) ? 'square_thumbnail' : 'thumbnail';
+                echo $this->formSelect($formStem . '[options][gallery-file-size]',
+                (@$options['gallery-file-size']) ? @$options['gallery-file-size'] : $defaultFileSize, array(),
+                array(
+                    'square_thumbnail' => __('Square Thumbnail'),
+                    'thumbnail' => __('Thumbnail'),
+                ));
+                
+            ?>
+        </div>
+        
+        <div class="captions-position">
+            <?php echo $this->formLabel($formStem . '[options][captions-position]', __('Captions position')); ?>
+            <?php
+            echo $this->formSelect($formStem . '[options][captions-position]',
+                @$options['captions-position'], array(),
+                array(
+                    'center' => __('Center'),
+                    'left' => __('Left'),
+                    'right' => __('Right')
+                ));
+            ?>
+        </div>
     </div>
 </div>
