412a682281f965c91022d1e59bd3fae2e507813c
Kim Nguyen <design@kimisgold.com>
2023-04-06 14:30:01 -0400

    Match advanced search markup to core pattern.
    omeka/Omeka#1006

--- views/shared/exhibits/exhibit-builder-advanced-search.php
+++ views/shared/exhibits/exhibit-builder-advanced-search.php
@@ -1,8 +1,6 @@
 <div id="search-by-exhibit" class="field">
-    <div class="two columns alpha">
-        <?php echo $this->formLabel('exhibit', __('Search by Exhibit')); ?>
-    </div>
-    <div class="five columns omega inputs">
+    <?php echo $this->formLabel('exhibit', __('Search by Exhibit')); ?>
+    <div class="inputs">
         <?php echo $this->formSelect('exhibit', @$_GET['exhibit'], array(), get_table_options('Exhibit')); ?>
     </div>
 </div>
