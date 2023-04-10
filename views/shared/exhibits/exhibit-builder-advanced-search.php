2ac5e5091fab8b00ede5c69b57cba72e65d5ccae
Kim Nguyen <design@kimisgold.com>
2023-04-10 16:12:03 -0400

    Fixed advanced search field markup.

--- views/shared/exhibits/exhibit-builder-advanced-search.php
+++ views/shared/exhibits/exhibit-builder-advanced-search.php
@@ -1,5 +1,5 @@
 <div id="search-by-exhibit" class="field">
-    <?php echo $this->formLabel('exhibit', __('Search by Exhibit')); ?>
+    <div class="field-meta"><?php echo $this->formLabel('exhibit', __('Search by Exhibit')); ?></div>
     <div class="inputs">
         <?php echo $this->formSelect('exhibit', @$_GET['exhibit'], array(), get_table_options('Exhibit')); ?>
     </div>
